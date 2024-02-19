b7a915c42f5809714e9537ef949df4434a2da47c
Dante Arrighi <dantearrighi@gmail.com>
2024-02-19 14:54:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ShoppingCart',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
