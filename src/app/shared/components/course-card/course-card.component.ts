92d1b550faf91defddd262b4b2dd7c813ab4a0a2
Dante Arrighi <dantearrighi@gmail.com>
2024-02-22 11:39:23 -0300

    WIP: course-card.component> Added prize and discounted prize

--- src/app/shared/components/course-card/course-card.component.ts
+++ src/app/shared/components/course-card/course-card.component.ts
@@ -10,6 +10,8 @@ import {
   starOutline,
 } from 'ionicons/icons';
 
+const CURRENCY_SYMBOL = '₹';
+
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
@@ -28,6 +30,23 @@ export class CourseCardComponent {
       arrowRedoOutline,
     });
   }
+
+  cleanNumber(value: string): number {
+    return parseFloat(value.replace(/[^0-9.]/g, ''));
+  }
+
+  get prizeToShow() {
+    const actualPrize = this.cleanNumber(this.course.actualPrice);
+    const discount = this.cleanNumber(this.course.discountPercentage);
+
+    if (discount > 0) {
+      const discountedPrize = actualPrize - actualPrize * (discount / 100);
+      return `${CURRENCY_SYMBOL}${discountedPrize.toFixed(2)}`;
+    } else {
+      return this.course.actualPrice;
+    }
+  }
+
   goToCourse() {
     console.log('Go to course');
   }
